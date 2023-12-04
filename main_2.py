7e3e7a2273ec206a5c7b90c503799f60fe8be443
Evgeniy Knyazev <e.s.knyazev@mtuci.ru>
2023-12-04 16:09:11 +0300

    вариант с рисованием трех квадратов

--- main_2.py
+++ main_2.py
@@ -3,34 +3,37 @@ from ultralytics.yolo.utils.plotting import Annotator  # ultralytics.yolo.utils.
 import cv2
 
 
-def are_rectangles_intersecting(rect1, rect2):
-    """Функция для определения пересечения двух прямоугольников"""
+def are_rectangles_intersecting(rect1, rect2, rect3, rectx):
+    """Функция для определения пересечения прямоугольников"""
     x1, y1, w1, h1 = rect1
     x2, y2, w2, h2 = rect2
+    x3, y3, w3, h3 = rect3
+    xx, yx, wx, hx = rectx
 
-    if x1 < x2 + w2 and x1 + w1 > x2 and y1 < y2 + h2 and y1 + h1 > y2:
-        cv2.putText(frame, 'Опасность!', (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+    if x1 < xx + wx and x1 + w1 > xx and y1 < yx + hx and y1 + h1 > yx:
+        return 1
+    elif x2 < xx + wx and x2 + w2 > xx and y2 < yx + hx and y2 + h2 > yx:
+        return 2
+    elif x3 < xx + wx and x3 + w3 > xx and y3 < yx + hx and y3 + h3 > yx:
+        return 3
     else:
-        cv2.putText(frame, 'Опасности нет', (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+        return False
 
 
-rectangle_1 = (100, 100, 100, 100)  # основная рамка (x, y, width, height)
-# rectangle_2 = (150, 150, 150, 150)  # основная рамка (x, y, width, height)
-# rectangle_3 = (200, 200, 200, 200)  # основная рамка (x, y, width, height)
+rectangle_1 = (300, 200, 100, 100)  # основная рамка (x, y, width, height)
+rectangle_2 = (250, 150, 200, 200)  # основная рамка (x, y, width, height)
+rectangle_3 = (200, 100, 300, 300)  # основная рамка (x, y, width, height)
 
-
-
-
-rectangle_x = []
+rectangle_x = []  # для обнаруженных рамок при сравнении
 
 model = YOLO('C:/Users/knyaz/PycharmProjects/yolo_cuda/runs/detect/test_hand_v4_2_nano/weights/best.pt')  # веса
 url_ip = 'http://192.168.0.102:8080/video'  # адрес камеры
 
 # cap = cv2.VideoCapture('IMG_4298.MOV')  # ролик
-# cap = cv2.VideoCapture(url_ip)  # ip камера
-cap = cv2.VideoCapture(0)  # веб-камера
+cap = cv2.VideoCapture(url_ip)  # ip камера
+# cap = cv2.VideoCapture(0)  # веб-камера
 
-# для изменения разрешения
+# # для изменения разрешения
 # new_width = 400
 # new_height = 700
 
@@ -39,8 +42,6 @@ while True:
 
     # изменение разрешения
     # frame = cv2.resize(frame, (new_width, new_height))
-    center_x = frame.shape[1] // 2
-    center_y = frame.shape[0] // 2
 
     results = model.predict(frame)
     for r in results:
@@ -55,12 +56,26 @@ while True:
             c = box.cls
             annotator.box_label(b, model.names[int(c)])
 
-            are_rectangles_intersecting(rectangle_1, rectangle_x)
+            intersection_result = are_rectangles_intersecting(rectangle_1,rectangle_2, rectangle_3, rectangle_x)
+            if intersection_result == 1:
+                cv2.putText(frame, 'Опасность!', (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+            elif intersection_result == 2:
+                cv2.putText(frame, 'Опасность!', (150, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+            elif intersection_result == 3:
+                cv2.putText(frame, 'Опасность!', (250, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+            else:
+                cv2.putText(frame, 'Опасности нет', (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 0), 2, cv2.LINE_AA)
+
             rectangle_x = []
 
     frame = annotator.result()
     frame = cv2.rectangle(frame, (rectangle_1[0], rectangle_1[1]), (rectangle_1[0] + rectangle_1[2], rectangle_1[1] + rectangle_1[3]), (0, 0, 255),
                           thickness=2, lineType=8, shift=0)
+    frame = cv2.rectangle(frame, (rectangle_2[0], rectangle_2[1]), (rectangle_2[0] + rectangle_2[2], rectangle_2[1] + rectangle_2[3]), (0, 255, 0),
+                          thickness=2, lineType=8, shift=0)
+    frame = cv2.rectangle(frame, (rectangle_3[0], rectangle_3[1]), (rectangle_3[0] + rectangle_3[2], rectangle_3[1] + rectangle_3[3]), (255, 0, 0),
+                          thickness=2, lineType=8, shift=0)
+
 
     cv2.imshow('YOLO V8 Detection', frame)
     if cv2.waitKey(1) & 0xFF == ord(' '):
