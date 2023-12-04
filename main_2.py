955a3b5b8c86fd3aa9bf2714224f54f9a6495994
Evgeniy Knyazev <e.s.knyazev@mtuci.ru>
2023-12-04 18:51:17 +0300

    рефакторинг

--- main_2.py
+++ main_2.py
@@ -29,8 +29,8 @@ rectangle_x = []  # для обнаруженных рамок при сравн
 model = YOLO('C:/Users/knyaz/PycharmProjects/yolo_cuda/runs/detect/test_hand_v4_2_nano/weights/best.pt')  # веса
 url_ip = 'http://192.168.0.102:8080/video'  # адрес камеры
 
-# cap = cv2.VideoCapture('IMG_4298.MOV')  # ролик
-cap = cv2.VideoCapture(url_ip)  # ip камера
+cap = cv2.VideoCapture('IMG_4298.MOV')  # ролик
+# cap = cv2.VideoCapture(url_ip)  # ip камера
 # cap = cv2.VideoCapture(0)  # веб-камера
 
 # # для изменения разрешения
@@ -45,18 +45,18 @@ while True:
 
     results = model.predict(frame)
     for r in results:
-
+        print(results)
         annotator = Annotator(frame)
-
         boxes = r.boxes
         for box in boxes:
+
             b = box.xyxy[0]  # get box coordinates in (top, left, bottom, right) format
             x, y, w, h = map(int, b.cpu().numpy().tolist())  # получение координат рамок объекта
             rectangle_x.extend([x, y, w, h])
             c = box.cls
             annotator.box_label(b, model.names[int(c)])
 
-            intersection_result = are_rectangles_intersecting(rectangle_1,rectangle_2, rectangle_3, rectangle_x)
+            intersection_result = are_rectangles_intersecting(rectangle_1, rectangle_2, rectangle_3, rectangle_x)
             if intersection_result == 1:
                 cv2.putText(frame, 'Опасность!', (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
             elif intersection_result == 2:
@@ -64,18 +64,20 @@ while True:
             elif intersection_result == 3:
                 cv2.putText(frame, 'Опасность!', (250, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
             else:
-                cv2.putText(frame, 'Опасности нет', (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 0), 2, cv2.LINE_AA)
+                cv2.putText(frame, 'Опасности нет', (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 55, 0), 2, cv2.LINE_AA)
 
             rectangle_x = []
 
     frame = annotator.result()
-    frame = cv2.rectangle(frame, (rectangle_1[0], rectangle_1[1]), (rectangle_1[0] + rectangle_1[2], rectangle_1[1] + rectangle_1[3]), (0, 0, 255),
-                          thickness=2, lineType=8, shift=0)
-    frame = cv2.rectangle(frame, (rectangle_2[0], rectangle_2[1]), (rectangle_2[0] + rectangle_2[2], rectangle_2[1] + rectangle_2[3]), (0, 255, 0),
-                          thickness=2, lineType=8, shift=0)
-    frame = cv2.rectangle(frame, (rectangle_3[0], rectangle_3[1]), (rectangle_3[0] + rectangle_3[2], rectangle_3[1] + rectangle_3[3]), (255, 0, 0),
-                          thickness=2, lineType=8, shift=0)
-
+    frame = cv2.rectangle(frame, (rectangle_1[0], rectangle_1[1]),
+                          (rectangle_1[0] + rectangle_1[2], rectangle_1[1] + rectangle_1[3]),
+                          (0, 0, 255), thickness=2, lineType=8, shift=0)
+    frame = cv2.rectangle(frame, (rectangle_2[0], rectangle_2[1]),
+                          (rectangle_2[0] + rectangle_2[2], rectangle_2[1] + rectangle_2[3]),
+                          (0, 255, 0), thickness=2, lineType=8, shift=0)
+    frame = cv2.rectangle(frame, (rectangle_3[0], rectangle_3[1]),
+                          (rectangle_3[0] + rectangle_3[2], rectangle_3[1] + rectangle_3[3]),
+                          (255, 0, 0), thickness=2, lineType=8, shift=0)
 
     cv2.imshow('YOLO V8 Detection', frame)
     if cv2.waitKey(1) & 0xFF == ord(' '):
