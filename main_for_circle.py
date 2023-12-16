a9e26865f72f7cc79548cf4af8904362f2d8e477
Evgeniy Knyazev <e.s.knyazev@mtuci.ru>
2023-12-16 13:10:46 +0300

    предфинал

--- main_for_circle.py
+++ main_for_circle.py
@@ -1,7 +1,12 @@
 from ultralytics import YOLO
 from ultralytics.yolo.utils.plotting import Annotator  # ultralytics.yolo.utils.plotting is deprecated
 import cv2
-from time import time
+import time
+from kuka import Kuka
+from openshowvar import *
+
+robot = openshowvar(ip = '192.168.1.2', port = 7000)
+kuka = Kuka(robot)
 
 
 # Функция для проверки пересечения кругов и прямоугольника
@@ -24,9 +29,9 @@ def check_intersection(circle_centers, circle_radius, rectangle_x):
     return -1
 
 
-circle_x_0, circle_y_0, circle_r_0 = 250, 250, 100
-circle_x_1, circle_y_1, circle_r_1 = 250, 250, 200
-circle_x_2, circle_y_2, circle_r_2 = 250, 250, 300
+circle_x_0, circle_y_0, circle_r_0 = 525, 400, 200
+circle_x_1, circle_y_1, circle_r_1 = 525, 400, 300
+circle_x_2, circle_y_2, circle_r_2 = 525, 400, 400
 
 circle_centers = [(circle_x_0, circle_y_0), (circle_x_1, circle_y_1), (circle_x_2, circle_y_2)]
 circle_radius = [circle_r_0, circle_r_1, circle_r_2]
@@ -38,15 +43,15 @@ model = YOLO('C:/Users/knyaz/PycharmProjects/yolo_cuda/runs/detect/hand_v5_mediu
 url_ip = 'http://192.168.0.102:8080/video'  # адрес камеры
 
 # cap = cv2.VideoCapture('IMG_4298.MOV')  # ролик
-cap = cv2.VideoCapture(url_ip)  # ip камера
-# cap = cv2.VideoCapture(0)  # веб-камера
-
+# cap = cv2.VideoCapture(url_ip)  # ip камера
+cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)  # веб-камера
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 # # для изменения разрешения
 # new_width = 400
 # new_height = 700
 intersection_result = 0
 while True:
-    start_time = time()
     _, frame = cap.read()
     count_box = [0]  # переменная для записи данных с функции по пересечению
 
@@ -64,21 +69,34 @@ while True:
             rectangle_x.extend([x, y, w, h])
             c = box.cls
             annotator.box_label(b, model.names[int(c)])
-
             intersection_result = check_intersection(circle_centers, circle_radius, rectangle_x)
-            print(intersection_result)
             count_box.append(4 - intersection_result)
-            print(count_box)
             rectangle_x = []
 
     if max(count_box) == 3:
         cv2.putText(frame, 'Опасность!', (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+        if box != []:
+            kuka.robot.write("$OV_PRO", str(0), False)
+            second = int(time.time())
+        if (int(time.time()) - second) >= 2:
+            kuka.robot.write("$OV_PRO", str(100), False)
     elif max(count_box) == 2:
         cv2.putText(frame, 'Опасность!', (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+        if box != []:
+            kuka.robot.write("$OV_PRO", str(50), False)
+            second = int(time.time())
+        if (int(time.time()) - second) >= 2:
+            kuka.robot.write("$OV_PRO", str(100), False)
     elif max(count_box) == 1:
         cv2.putText(frame, 'Опасность!', (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+        if box != []:
+            kuka.robot.write("$OV_PRO", str(75), False)
+            second = int(time.time())
+        if (int(time.time()) - second) >= 2:
+            kuka.robot.write("$OV_PRO", str(100), False)
     else:
         cv2.putText(frame, 'Опасности нет', (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 55, 0), 2, cv2.LINE_AA)
+        kuka.robot.write("$OV_PRO", str(100), False)
 
     intersection_result = 0
 
@@ -87,9 +105,6 @@ while True:
     cv2.circle(frame, (circle_x_1, circle_y_1), circle_r_1, (0, 255, 0), 2)
     cv2.circle(frame, (circle_x_2, circle_y_2), circle_r_2, (255, 0, 0), 2)
 
-    end_time = time()
-    fps = 1 /round(end_time - start_time, 2)
-    print('FPS: ' + str(fps))
     cv2.imshow('YOLO V8 Detection', frame)
     if cv2.waitKey(1) & 0xFF == ord(' '):
         break
